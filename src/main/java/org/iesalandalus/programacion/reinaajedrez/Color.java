8a1d4809a7fd49f93bd4b5531fd5170d6761f244
tzeen <tzeen@DESKTOP-2LP53EJ.Home>
2022-11-13 13:18:15 +0100

    commit 3; se crea el constructor para la string

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -3,5 +3,9 @@ package org.iesalandalus.programacion.reinaajedrez;
 public enum Color {
 	BLANCO("blanco"), NEGRO("negro");
 	private String cadenaAMostrar;
+	private Color (String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
 
 }
