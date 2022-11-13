23fff351effeaf9bd72a7603d406ef69001313fe
tzeen <tzeen@DESKTOP-2LP53EJ.Home>
2022-11-13 14:13:09 +0100

    Commit posición 3;
    Se crear los getters y setters con sus correspondientes excepciones.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -2,7 +2,35 @@ package org.iesalandalus.programacion.reinaajedrez;
 
 public class Posicion {
 private int fila;
+
 private char columna;
-	
+
+	/*getters */
+public int getFila() {
+	return this.fila;
+}
+public char getColumna() {
+	return this.columna;
+}
+
+	private void setFila(int fila) {
+		if (fila<1 || fila>8) {
+			throw new IllegalArgumentException("ERROR; valor de fila no valido");
+			}
+		else this.fila = fila;
+	}
+
+
+	private void setColumna(char columna) {
+
+		int columnNumero = columna;
+			if(columnNumero<1 || columnNumero>8) {
+				throw new IllegalArgumentException("ERROR; valor de columna no valido");
+			}
+		else this.columna=columna;
+		
+		}
 	
 }
+
+
