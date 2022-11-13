633c6ac28e831a45e0da32ab0d53ae4d9f463d42
tzeen <tzeen@DESKTOP-2LP53EJ.Home>
2022-11-13 13:07:08 +0100

    Commit 2. Se ha creado la enum Direccion, pero, con los requisitos que se le piden a Color también, como se dijo en clase.

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Direccion.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.reinaajedrez;
+
+public enum Direccion {
+
+		
+	/* para que pase los tests como se dijo en clase, se hará directamente como la enum color */
+		NORTE("Norte"), NORESTE("Noreste"), ESTE("Este"), SURESTE("Sureste"), SUR("Sur"), SUROESTE("Suroeste"), OESTE("Oeste"), NOROESTE("Noroeste");
+
+		private String cadenaAMostrar;
+
+		private Direccion(String cadenaAMostrar) {
+			this.cadenaAMostrar = cadenaAMostrar;
+		}
+
+		/*aquí hacemos override al toString por defecto,devolviendo el valor de la cadena (nombre de la direccion) */
+		public String toString() {
+			return cadenaAMostrar;
+		}
+}
+
+
