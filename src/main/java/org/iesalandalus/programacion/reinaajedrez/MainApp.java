09b59413a3559b1c0e093fe76a0fa81b5318565b
tzeen <tzeen@192.168.56.1>
2022-11-14 14:19:51 +0100

    commit mainapp	5
    se crea el metodo mover, que pasa a la reina el resultado de invocar los
    metodos de consola para direccion y pasos.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,5 +1,8 @@
 package org.iesalandalus.programacion.reinaajedrez;
 import org.iesalandalus.programacion.utilidades.Entrada;
+
+import javax.naming.OperationNotSupportedException;
+
 import org.iesalandalus.programacion.reinaajedrez.modelo.*;
 
 
@@ -45,6 +48,13 @@ public class MainApp {
 
 		reina = new Reina(Consola.elegirColor());
 		
+	}
+
+	private static void mover() throws OperationNotSupportedException {
+		
+		Consola.menuDirecciones();
+
+		reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
 	}
 		
 	public static void main(String[] args) {
