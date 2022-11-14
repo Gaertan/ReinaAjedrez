1d4ce1e5d610c6950c22f0eb28acfef3606f1d89
tzeen <tzeen@192.168.56.1>
2022-11-14 14:01:34 +0100

    commit mainApp
    Creo ejecutarOpcion.
    
    Como todavia no tengo muy claro las opciones que van a haber/lo que va a
    hacer(solo se que son 4 por el resto de enunciados), dejo el método
    vacío y lo rellenaré conforme haga las opciones en sí

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -4,10 +4,49 @@ import org.iesalandalus.programacion.reinaajedrez.modelo.*;
 
 
 public class MainApp {
-
+	
+	Reina reina;	
+		
+	private static void ejecutarOpcion(int opcion) {
+		
+		switch(opcion){
+		
+		case 1:{
+			
+			break;
+			}
+			
+		case 2:{	
+			
+			break;
+			}
+			
+		case 3:{			
+			break;
+			}
+			
+		case 4:{
+			
+			break;
+		}
+		
+		
+		}
+		
+		
+	}
+		
 	public static void main(String[] args) {
 
-	private Reina reina;
-			
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
