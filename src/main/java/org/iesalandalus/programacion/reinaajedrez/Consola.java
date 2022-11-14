c2c42e8a66c5c53a4b9efac3bd60c3d03929b38a
tzeen <tzeen@192.168.56.1>
2022-11-14 12:42:24 +0100

    consola commit 3
    creado el metodo mostrarMenu

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,5 +1,19 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public class Consola {
+	
+	
+private Consola(){	
+}
+
+public static void mostrarMenu(){
+	System.out.println("1. Crear la reina default.");
+	System.out.println("2. Crear reina escogiendo el color.");
+	System.out.println("3. Mover.");
+	System.out.println("4. Salir.");
+}
+
+
+
 
 }
