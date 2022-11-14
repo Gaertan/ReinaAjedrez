7501ae445953003d99be95abbdff29423cf881ee
tzeen <tzeen@192.168.56.1>
2022-11-14 13:39:09 +0100

    commit mainapp 1;
    Añadido el atributo reina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,13 @@
 package org.iesalandalus.programacion.reinaajedrez;
+import org.iesalandalus.programacion.utilidades.Entrada;
+import org.iesalandalus.programacion.reinaajedrez.modelo.*;
+
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
+	private Reina reina;
+			
 	}
 }
